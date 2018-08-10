b77274a7bdec8ec92515503831ea88561c13f245
Larry D. Lee jr <larry_lee@elucidsolutions.com>
2018-08-10 10:49:59 -0400

    Update core.d
    Reformtted the core module.

--- kakuro/core.d
+++ kakuro/core.d
@@ -1,10 +1,9 @@
 /++
-	Synopsis:
-		This module defines class that represent
-		kakuro puzzles, elements, and sequences.
+  This module defines class that represent
+  kakuro puzzles, elements, and sequences.
 
-		These classes can be used to resolve
-		kakuro puzzles, sequences, and elements.
+  These classes can be used to resolve
+  kakuro puzzles, sequences, and elements.
 ++/
 
 module kakuro.core;
@@ -17,144 +16,124 @@ import std.perf;
 import std.cstream;
 
 /// sequence elements:
-class Elem
-{
-	Seq[] seqs; /// sequences.
-	real[] vals; /// values.
+class Elem {
+  Seq[] seqs; /// sequences.
+  real[] vals; /// values.
 
-	/// defines an element:
-	this () {}
+  /// defines an element:
+  this () {}
 
-	/// defines an element:
-	this (Seq[] seqs, real[] vals)
-	{
-		this.seqs = seqs;
-		this.vals = vals;
-	}
+  /// defines an element:
+  this (Seq[] seqs, real[] vals) {
+    this.seqs = seqs;
+    this.vals = vals;
+  }
 
-	/// initialize values set:
-	void init ()
-	{
-		this.vals = set.intersection
-		(
-			this.seqs [0].vals,
-			this.seqs [1].vals
-		);
-	}
+  /// initialize values set:
+  void init () {
+    this.vals = set.intersection (
+      this.seqs [0].vals,
+      this.seqs [1].vals
+    );
+  }
 }
 
 /// sequences:
-class Seq
-{
-	real sum;     /// sum.
-	Elem[] elems; /// elements.
-	real[] vals;  /// set of possible element values.
+class Seq {
+  real sum;     /// sum.
+  Elem[] elems; /// elements.
+  real[] vals;  /// set of possible element values.
 
-	/// defines a sequence:
-	this (real sum, Elem[] elems, real[] vals)
-	{
-		this.sum = sum;
-		this.elems = elems;
+  /// defines a sequence:
+  this (real sum, Elem[] elems, real[] vals) {
+    this.sum = sum;
+    this.elems = elems;
 
-		foreach (ref elem; elems)
-		{
-			elem.seqs ~= this;
-		}
+    foreach (ref elem; elems) {
+      elem.seqs ~= this;
+    }
 
-		this.vals = vals;
-	}
+    this.vals = vals;
+  }
 
-	/// defines a sequence:
-	this (Elem[] elems)
-	{
-		this (0, elems, [9, 8, 7, 6, 5, 4, 3, 2, 1]);
-	}
+  /// defines a sequence:
+  this (Elem[] elems) {
+    this (0, elems, [9, 8, 7, 6, 5, 4, 3, 2, 1]);
+  }
 
-	/// defines a sequence:
-	this (real sum, Elem[] elems)
-	{
-		this (sum, elems, kakuro.seqs.elems (sum, elems.length));
-	}
+  /// defines a sequence:
+  this (real sum, Elem[] elems) {
+    this (sum, elems, kakuro.seqs.elems (sum, elems.length));
+  }
 
-	/// tries to resolve this sequence and returns true
-	/// if it reduced the number of possible element values:
-	bool resolve ()
-	{
-		uint nvals;
+  /// tries to resolve this sequence and returns true
+  /// if it reduced the number of possible element values:
+  bool resolve () {
+    uint nvals;
 
-		// get the array that represents this.elems:
-		
-		real[][] elems = new real [][this.elems.length];
+    // I. get the array that represents this.elems:
+    
+    real[][] elems = new real [][this.elems.length];
 
-		for (uint i; i < this.elems.length; i ++)
-		{
-			nvals += this.elems [i].vals.length;
-			elems [i] = this.elems [i].vals;
-			this.elems [i].vals = new real [0];
-		}
+    for (uint i; i < this.elems.length; i ++) {
+      nvals += this.elems [i].vals.length;
+      elems [i] = this.elems [i].vals;
+      this.elems [i].vals = new real [0];
+    }
 
-		// resolve this sequence:
+    // II. resolve this sequence:
 
-		real[][] res = kakuro.seqs.seqs (this.sum, elems);
+    real[][] res = kakuro.seqs.seqs (this.sum, elems);
 
-		// update the element values:
+    // III. update the element values:
 
-		uint _nvals;
+    uint _nvals;
 
-		for (uint i; i < res.length; i ++)
-		{
-			_nvals += res [i].length;
+    for (uint i; i < res.length; i ++) {
+      _nvals += res [i].length;
 
-			for (uint j; j < res [i].length; j ++)
-			{
-				this.elems [j].vals = set.add (this.elems [j].vals, res [i][j]);
-			}
-		}
+      for (uint j; j < res [i].length; j ++) {
+        this.elems [j].vals = set.add (this.elems [j].vals, res [i][j]);
+      }
+    }
 
-		// return status:
-
-		return _nvals < nvals;
-	}
+    // IV. return status:
+    return _nvals < nvals;
+  }
 }
 
 /// puzzle:
-class Puzzle
-{
-	Elem[] elems; /// elements.
-	Seq[]  seqs;  /// sequences.
+class Puzzle {
+  Elem[] elems; /// elements.
+  Seq[]  seqs;  /// sequences.
 
-	/// defines a puzzle:
-	this (Elem[] elems, Seq[] seqs)
-	{
-		this.elems = elems;
-		this.seqs  = seqs;
-		this.init ();
-	}
+  /// defines a puzzle:
+  this (Elem[] elems, Seq[] seqs) {
+    this.elems = elems;
+    this.seqs  = seqs;
+    this.init ();
+  }
 
-	/// initialize:
-	void init ()
-	{
-		// initialize element values:
-		foreach (Elem elem; this.elems) { elem.init (); }
-	}
+  /// initialize:
+  void init () {
+    // initialize element values:
+    foreach (Elem elem; this.elems) { elem.init (); }
+  }
 
-	/// tries to resolve the sequences and returns
-	/// true if it resolved at least one of the sequences:
-	bool resolve ()
-	{
-		bool reduced;
+  /// tries to resolve the sequences and returns
+  /// true if it resolved at least one of the sequences:
+  bool resolve () {
+    bool reduced;
 
-		foreach (Seq seq; this.seqs)
-		{
-			if (seq.sum != 0 && seq.resolve ()) reduced = true;
-		}
+    foreach (Seq seq; this.seqs) {
+      if (seq.sum != 0 && seq.resolve ()) reduced = true;
+    }
 
-		return reduced;
-	}
+    return reduced;
+  }
 
-	/// tries to solve this puzzle:
-	void solve ()
-	{
-		while (this.resolve ()) {}
-	}
+  /// tries to solve this puzzle:
+  void solve () {
+    while (this.resolve ()) {}
+  }
 }
